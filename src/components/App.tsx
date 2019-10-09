31f291e151ae0318199c3aa581d0c6142f2d0dc2
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-10-09 01:16:02 -0700

    DIsplay with better graphics

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,19 @@ import './App.css';
 import { todoInitialState, todoReducer } from '../store/todos/reducers';
 import { ListItem, List } from '@material-ui/core';
 import { Layout } from './Layout'
+import { TodoList } from './TodoList'
 
 const App = memo(props => {
   const [state, dispatch] = useReducer(todoReducer, todoInitialState)
   return (<Layout>
-    <List>
+    <TodoList
+      todos={state.allIds.map(id => state.getById[id])}
+      onItemCheck={() => {}}
+      onItemRemove={() => {}}
+    />
+    {/* <List>
       {state.allIds.map(id => <ListItem>{state.getById[id].text}</ListItem>)}
-    </List>
+    </List> */}
   </Layout>)
 })
 
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,64 @@
+import React, { memo } from 'react';
+
+import {
+  List,
+  ListItem,
+  Checkbox,
+  IconButton,
+  ListItemText,
+  ListItemSecondaryAction,
+  Paper,
+  Grid,
+} from '@material-ui/core'
+import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
+import { Todo } from '../store/todos/actions';
+
+interface ITodoProps {
+  onCheckBoxToggle: () => void
+  onButtonClick: () => void
+  completed: boolean
+  text: string
+  divider: boolean
+}
+
+const TodoListItem = memo((props: ITodoProps) => (
+  <ListItem divider={props.divider}>
+    <Checkbox
+      onClick={props.onCheckBoxToggle}
+      checked={props.completed}
+      disableRipple
+    />
+    <ListItemText primary={props.text} />
+    <ListItemSecondaryAction>
+      <IconButton arial-label='Delete Todo' onClick={props.onButtonClick}>
+        <DeleteOutlined />
+      </IconButton>
+    </ListItemSecondaryAction>
+  </ListItem>
+))
+
+interface ITooListProps {
+  todos: Todo[]
+  onItemRemove: (id: number) => void
+  onItemCheck: (id: number) => void
+}
+
+export const TodoList = memo((props: ITooListProps) => (
+  <>
+    {props.todos.length > 0 && (
+      <Paper style={{margin: 16}}>
+        <List style={{ overflow: 'scroll'}}>
+          {props.todos.map((todo, id) => (
+            <TodoListItem
+              { ...todo }
+              key={`TodoItem.${id}`}
+              divider={id !== props.todos.length - 1}
+              onButtonClick={() => props.onItemRemove(id)}
+              onCheckBoxToggle={() => props.onItemCheck(id)}
+            />
+          ))}
+        </List>
+      </Paper>
+    )}
+  </>
+))
