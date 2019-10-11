8bc69465f4166b3eeb6edb2cc6e7338d19c17cf0
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-10-11 16:43:00 -0700

    simplify interface

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,27 +10,26 @@ import {
   Paper,
 } from '@material-ui/core'
 import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
-import { Todo, toggleTodo, deleteTodo } from '../store/todos/actions';
+import { toggleTodo, deleteTodo } from '../store/todos/actions';
 import { useReduxSelector } from '../store';
 import { useDispatch } from 'react-redux';
 
 interface ITodoProps {
   id: number
-  completed: boolean
-  text: string
   divider: boolean
 }
 
 const TodoListItem = memo((props: ITodoProps) => {
+  const todo = useReduxSelector(state => state.todo.getById[props.id])
   const dispatch = useDispatch()
 
   return <ListItem divider={props.divider}>
     <Checkbox
       onClick={() => dispatch(toggleTodo(props.id))}
-      checked={props.completed}
+      checked={todo.completed}
       disableRipple
     />
-    <ListItemText primary={props.text} />
+    <ListItemText primary={todo.text} />
     <ListItemSecondaryAction>
       <IconButton arial-label='Delete Todo' onClick={() => dispatch(deleteTodo(props.id))}>
         <DeleteOutlined />
@@ -40,18 +39,16 @@ const TodoListItem = memo((props: ITodoProps) => {
 })
 
 export const TodoList = () => {
-  const todoState = useReduxSelector(state => state.todo)
-  const todos = todoState.allIds.map(id => todoState.getById[id])
+  const ids = useReduxSelector(state => state.todo.allIds)
 
-  if (todos.length > 0) {
+  if (ids.length > 0) {
     return <Paper style={{margin: 16}}>
       <List style={{ overflow: 'scroll'}}>
-        {todos.map((todo, id) => (
+        {ids.map((id, i) => (
           <TodoListItem
-            { ...todo }
             key={`TodoItem.${id}`}
             id={id}
-            divider={id !== todos.length - 1}
+            divider={i !== ids.length - 1}
           />
         ))}
       </List>
