828bd9112fbe87daa71c28860400403f796b2581
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-10-16 15:38:32 -0700

    Styling for TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
-import { List, Paper } from '@material-ui/core'
+import { List, Paper, Theme } from '@material-ui/core'
 import { useReduxSelector, Status, State, stateStatusSelector, stateQuerySelector } from '../store';
 import { TodoItem } from './TodoItem'
+import { makeStyles } from '@material-ui/styles';
+
+
+const useStyles = makeStyles((theme: Theme) => ({
+  paper: {
+    margin: theme.spacing(2)
+  },
+  list: {
+    backgroundColor: theme.palette.background.paper,
+  },
+}))
 
 function todoListSelector(state: State) {
   const status = stateStatusSelector(state)
@@ -29,19 +40,20 @@ function todoListSelector(state: State) {
 
 export const TodoList = () => {
   const ids = useReduxSelector(todoListSelector)
+  const classes = useStyles()
 
   if (ids.length > 0) {
-    return <Paper style={{margin: 16}}>
-      <List style={{ overflow: 'scroll'}}>
-        {ids.map((id, i) => (
-          <TodoItem
-            key={`TodoItem.${id}`}
-            id={id}
-            divider={i !== ids.length - 1}
-          />
-        ))}
-      </List>
-    </Paper>
+    return <Paper className={classes.paper}>
+        <List dense className={classes.list}>
+          {ids.map((id, i) => (
+            <TodoItem
+              key={`TodoItem.${id}`}
+              id={id}
+              divider={i !== ids.length - 1}
+            />
+          ))}
+        </List>
+      </Paper>
   } else {
     return null
   }
