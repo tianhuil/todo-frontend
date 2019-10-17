dbfe2e23332487282945a8bc7fad73a760b3316f
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-10-17 09:53:20 -0700

    Reducing console.log

--- src/store/user/handler.ts
+++ src/store/user/handler.ts
@@ -3,10 +3,8 @@ import { signIn, signOut, UserActionTypes } from "./action";
 
 export function userAuthStateChange(user: firebase.User | null): UserActionTypes {
   if (user) {
-    console.log('logged in')
     return signIn(user)
   } else {
-    console.log('unlogged in')
     return signOut()
   }
 }
