b5cf8dcc7004f5d7cc61dc9d3f1e4a9ab2c55337
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-10-20 22:29:49 -0700

    hashtags displayed as links

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import * as React from 'react'
 
 import { Id } from "../type"
 import { memo } from "react"
 import { useReduxSelector } from "../store"
 import { useDispatch } from "react-redux"
+import { Link as RouterLink } from 'react-router-dom'
 import { useTodoHandler } from "./Firestore"
-import { ListItem, Checkbox, ListItemText, ListItemSecondaryAction, IconButton, Theme, makeStyles } from "@material-ui/core"
+import { ListItem, Checkbox, ListItemText, ListItemSecondaryAction, IconButton, Theme, makeStyles, Link as MuiLink, Typography } from "@material-ui/core"
 import DeleteOutlined from '@material-ui/icons/DeleteOutlined'
 import Sync from '@material-ui/icons/Sync'
 
@@ -25,6 +26,40 @@ interface ITodoProps {
   divider: boolean
 }
 
+interface ILinkProps {
+  to: {
+    pathname?: string
+    search?: string
+    hash?: string
+  }
+}
+
+const ForwardedRouterLink = React.forwardRef<HTMLAnchorElement, ILinkProps>(
+  (props, ref) => <RouterLink innerRef={ref} {...props} />
+)
+
+const Link = (props: React.PropsWithChildren<ILinkProps>) => {
+  return <MuiLink component={ForwardedRouterLink} to={props.to}>
+    {props.children}
+  </MuiLink>
+}
+
+function linkHashtag(text: string): (string | JSX.Element)[] {
+  const words = text.split(' ').map(
+    word => word.startsWith('#')
+      ? <Link to={{hash: word}}>{word}</Link>
+      : word
+  )
+  const result = new Array(words.length * 2 - 1)
+  words.forEach((word, i) => {
+    result[2 * i] = word
+  })
+  for (let i=1; i< result.length; i += 2) {
+    result[i] = ' '
+  }
+  return result
+}
+
 export const TodoItem = memo((props: ITodoProps) => {
   const todo = useReduxSelector(state => state.todo.getById[props.id])
   const dispatch = useDispatch()
@@ -41,7 +76,11 @@ export const TodoItem = memo((props: ITodoProps) => {
       className={classes.checkbox}
       disableRipple
     />
-    <ListItemText primary={todo.data.text} />
+    <ListItemText>
+      <Typography>
+        {linkHashtag(todo.data.text)}
+      </Typography>
+    </ListItemText>
     <ListItemSecondaryAction>
       <IconButton
         arial-label='Delete Todo'
